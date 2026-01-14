040f90767bc6cf823548d2c9407bffa9b4ff524c
Sanyal Singh <sanyalsingh84@gmail.com>
2026-01-14 15:23:33 +0530

    feat: added better social links

--- src/components/SectionAboutMe.js
+++ src/components/SectionAboutMe.js
@@ -19,7 +19,8 @@ const SectionAboutMe = ({ proLine, addSection }) => {
         <h1
           ref={(el) => {
             aboutHead = el;
-          }}>
+          }}
+        >
           About ME
         </h1>
       </div>
@@ -28,26 +29,38 @@ const SectionAboutMe = ({ proLine, addSection }) => {
         className="pro-one"
         ref={(el) => {
           aboutSec = el;
-        }}>
+        }}
+      >
         <div className="project-title">
           <div className="heading left">
             <h1
               ref={(el) => {
                 aboutTitle = el;
-              }}>
+              }}
+            >
               Sanyal Singh
             </h1>
             <div
               className="about-line"
               ref={(el) => {
                 aboutLine = el;
-              }}></div>
+              }}
+            ></div>
             <div
               className="disc-about hide"
               ref={(el) => {
                 aboutDisc = el;
-              }}>
-              <h2>Software Engineer with over 3 years of professional experience, working on both frontend and backend development. On the frontend, I’ve built responsive user interfaces and dashboards using JavaScript and React. On the backend, I’ve contributed to building and maintaining systems with PHP, Laravel, and MySQL. I enjoy creating user-friendly solutions and thrive in collaborative environments where I can continue to learn and grow.
+              }}
+            >
+              <h2>
+                Software Engineer with over 3 years of professional experience,
+                working on both frontend and backend development. On the
+                frontend, I’ve built responsive user interfaces and dashboards
+                using JavaScript and React & Nextjs. On the backend, I’ve
+                contributed to building and maintaining systems with Node.js,
+                Express & MongoDB. I enjoy creating user-friendly solutions and
+                thrive in collaborative environments where I can continue to
+                learn and grow.
               </h2>
             </div>
           </div>
@@ -56,14 +69,17 @@ const SectionAboutMe = ({ proLine, addSection }) => {
           className="project-image image-5"
           ref={(el) => {
             aboutImage = el;
-          }}></div>
+          }}
+        ></div>
         <div className="disc-about show">
           <h2>
-            I am a web developer who’s in fourth semester of bachelor in
-            computer application degree. My friend got me into the world of web
-            development, I got instantly fascinated by it and then I decided to
-            pursue it. Since then, I've been trying to learn new technologies so
-            I can get closer to get a job in this field.
+            Software Engineer with over 3 years of professional experience,
+            working on both frontend and backend development. On the frontend,
+            I’ve built responsive user interfaces and dashboards using
+            JavaScript and React & Nextjs. On the backend, I’ve contributed to
+            building and maintaining systems with Node.js, Express, & MongoDB. I
+            enjoy creating user-friendly solutions and thrive in collaborative
+            environments where I can continue to learn and grow.
           </h2>
         </div>
       </article>
